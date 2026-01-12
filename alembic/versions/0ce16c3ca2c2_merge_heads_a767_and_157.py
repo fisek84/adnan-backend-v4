f2b50af33fe9eab8229ff92a0479b1dc645a8cc7
Ad Fisek <fisekovic.adnan@gmail.com>
2026-01-12 13:23:30 +0100

    chore: ruff-format alembic merge revision

--- alembic/versions/0ce16c3ca2c2_merge_heads_a767_and_157.py
+++ alembic/versions/0ce16c3ca2c2_merge_heads_a767_and_157.py
@@ -5,6 +5,7 @@ Revises: a767a3914519, 157956357fc4
 Create Date: 2026-01-12 13:17:49.291430
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '0ce16c3ca2c2'
-down_revision: Union[str, None] = ('a767a3914519', '157956357fc4')
+revision: str = "0ce16c3ca2c2"
+down_revision: Union[str, None] = ("a767a3914519", "157956357fc4")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
