53f290380b54c13d51b7c4fa4057f424e9d45577
Ad Fisek <fisekovic.adnan@gmail.com>
2026-01-13 00:45:21 +0100

    chore: ruff cleanup for alembic merge migrations

--- alembic/versions/af209f6a19e2_merge_heads_b19dd721c3e9_and_.py
+++ alembic/versions/af209f6a19e2_merge_heads_b19dd721c3e9_and_.py
@@ -5,15 +5,14 @@ Revises: b19dd721c3e9, b4fd3961bad5
 Create Date: 2026-01-12 23:04:49.697756
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'af209f6a19e2'
-down_revision: Union[str, None] = ('b19dd721c3e9', 'b4fd3961bad5')
+revision: str = "af209f6a19e2"
+down_revision: Union[str, None] = ("b19dd721c3e9", "b4fd3961bad5")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- alembic/versions/b19dd721c3e9_create_identity_root.py
+++ alembic/versions/b19dd721c3e9_create_identity_root.py
@@ -5,15 +5,14 @@ Revises: 01a578baf111
 Create Date: 2026-01-12 22:44:43.638541
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b19dd721c3e9'
-down_revision: Union[str, None] = '01a578baf111'
+revision: str = "b19dd721c3e9"
+down_revision: Union[str, None] = "01a578baf111"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
